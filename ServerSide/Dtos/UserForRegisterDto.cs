6861edd9a26a56e9d6bade5a379e0aafd21f7460
alexpodchikov <podchikov@gmail.com>
2020-07-20 21:31:43 +0300

    another bug fixing commit

--- ServerSide/Dtos/UserForRegisterDto.cs
+++ ServerSide/Dtos/UserForRegisterDto.cs
@@ -9,7 +9,7 @@ namespace ServerSide.Dtos
         public string Username { get; set; }
 
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characters.")]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "You must specify password at least 4 characters.")]
         public string Password { get; set; }
 
         [Required]
